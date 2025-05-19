5fe9eb54cfc0b59a798bb841b94f3d4281bc34cd
lauanderson-rael <lauanderson38@gmail.com>
2025-05-18 23:19:44 -0300

    criada pagina de cadastro de carro

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 # Create your views here.
 def cars_view(request):
@@ -14,6 +15,19 @@ def cars_view(request):
         'cars.html',
         {'cars': cars}
       )
+
+def new_car_view(request):
+   new_car_form = CarForm()
+   return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+
+
+
+
+
+
+
+
 
 
 # __ = navegacao entre tabelas que tem relacao
