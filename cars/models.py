c0c7b58d283ac6debaa56fb75cb73b397ba5eec2
lauanderson-rael <lauanderson38@gmail.com>
2025-07-03 13:50:11 -0300

    update signals

--- cars/models.py
+++ cars/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Brand(models.Model):
    id = models.AutoField(primary_key=True)
    name = models.CharField(max_length=200)
-
    def __str__(self):
       return self.name
+   
 
 class Car(models.Model):
    id = models.AutoField(primary_key=True)
@@ -16,6 +16,9 @@ class Car(models.Model):
    plate = models.CharField(max_length=10, blank=True, null=True)
    value = models.FloatField(blank=True, null=True)
    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+   bio = models.TextField(blank=True, null=True)
+   
+
 
    def __str__(self):
       return self.model # retorna o nome do carro
